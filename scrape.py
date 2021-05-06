0b31b5aa3d96b35149366157274c726b5db70a83
Josh <joshriches@gmail.com>
2021-05-06 11:04:56 -0600

    add test.html dummy data and built scraper

--- scrape.py
+++ scrape.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import lxml
 
-with open('file.html', 'r') as html_file:
+with open('test.html', 'r') as html_file:
     content = html_file.read()
 
     soup =  bs(content, 'lxml')
-    ps = soup.find_all('p')
-    print(ps)
-    for p in ps:
-        print(p.text)
+    titles = soup.find_all('title')
+    h1s = soup.find_all('h1')
+
+    for title in titles:
+        print(title.text)
+
+    for h1 in h1s:
+        print(h1.text)
